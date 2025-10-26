696738a1b5678e11e4dd047ee0319d859f3edcae
Andre Luiz <andrejarczewski@gmail.com>
2025-10-26 12:28:03 -0300

    tarefas

--- gerenciador.tarefas/src/ListaTarefas.java
+++ gerenciador.tarefas/src/ListaTarefas.java
@@ -3,5 +3,36 @@ import java.time.format.DateTimeFormatter;
 public class ListaTarefas {
     private int id;
     private LocalDate dataTarefa;
-    private 
+    private String descricaoTarefa;
+    private String observacao;
+    public int getId() {
+        return id;
+    }
+    public void setId(int id) {
+        this.id = id;
+    }
+    public LocalDate getDataTarefa() {
+        return dataTarefa;
+    }
+    public void setDataTarefa(LocalDate dataTarefa) {
+        this.dataTarefa = dataTarefa;
+    }
+    public String getDescricaoTarefa() {
+        return descricaoTarefa;
+    }
+    public void setDescricaoTarefa(String descricaoTarefa) {
+        this.descricaoTarefa = descricaoTarefa;
+    }
+    public String getObservacao() {
+        return observacao;
+    }
+    public void setObservacao(String observacao) {
+        this.observacao = observacao;
+    }
+    
+    public ListaTarefas(){}
+
+    public ListaTarefas( int id, LocalDate dataTarefa, String descricaoTarefa, String observacao){
+        
+    }
 }
